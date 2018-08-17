2b4c7d38cbf12b8e85e23c94d1019e2435a50bb7
Liebhardt Alexander <Alexander.Liebhardt@gateb.com>
2018-08-17 12:41:10 +0200

    Add smotth scrolling.

--- patterns/02-ui/pagescroller/pagescroller.js
+++ patterns/02-ui/pagescroller/pagescroller.js
@@ -1,13 +1,72 @@
 exports.init = (() => {
   const sections = document.querySelectorAll('a[pagescroller]');
 
+  const currentYPosition = () => {
+    // Firefox, Chrome, Opera, Safari
+    if (window.pageYOffset) {
+      return window.pageYOffset;
+    }
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop) {
+      return document.documentElement.scrollTop;
+    }
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) {
+      return document.body.scrollTop;
+    }
+    return 0;
+  };
+
+  const elmYPosition = (eID) => {
+    const elm = document.getElementById(eID);
+    let y = elm.offsetTop;
+    let node = elm;
+    while (node.offsetParent && node.offsetParent !== document.body) {
+      node = node.offsetParent;
+      y += node.offsetTop;
+    }
+    return y;
+  };
+
+  const smoothScrollTo = (eID) => {
+    const startY = currentYPosition();
+    const stopY = elmYPosition(eID);
+    const distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+      window.scrollTo(0, stopY);
+      return;
+    }
+    let speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    const step = Math.round(distance / 25);
+    let leapY = stopY > startY ? startY + step : startY - step;
+    let timer = 0;
+    if (stopY > startY) {
+      for (let i = startY; i < stopY; i += step) {
+        setTimeout(`window.scrollTo(0, ${leapY})`, timer * speed); // eslint-disable-line
+        leapY += step;
+        if (leapY > stopY) leapY = stopY;
+        timer++;
+      } return;
+    }
+    for (let i = startY; i > stopY; i -= step) {
+      setTimeout(`window.scrollTo(0, ${leapY})`, timer * speed); // eslint-disable-line
+      leapY -= step;
+      if (leapY < stopY) leapY = stopY;
+      timer++;
+    }
+  };
+
   const handleMenuClick = (e) => {
+    e.preventDefault();
     const target = ['a'].indexOf(e.target.nodeName.toLowerCase()) > -1 ? e.target : e.target.closest('a');
     const items = target.closest('ul').querySelectorAll('li');
     items.forEach((item) => {
       item.classList.remove('active');
     });
     target.closest('li').classList.add('active');
+    const eID = target.href.split('#')[1];
+    smoothScrollTo(eID);
   };
 
   const buildMenu = () => {
